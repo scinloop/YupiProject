8230d7a9a2f8ad0387f975ee5c46c1f65353da8d
scinloop <1151127150@qq.com>
2025-01-04 12:24:55 +0800

    quanjiao

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="basicLayout">
+    <a-layout>
+      <a-layout-header>
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://codefather.cn" target="_blank"> 变成导航链接 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 16px;
+}
+
+#basicLayout .content {
+  padding: 20px;
+  margin-bottom: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+}
+</style>
